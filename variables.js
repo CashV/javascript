a6ef27805a8d057e9f3c330899689f6e00ddd3c3
CashV <101281150+CashV@users.noreply.github.com>
2024-06-13 13:15:05 +0000

    variable config done

---
+++ variables.js
@@ -0,0 +1,8 @@
+const collegeId = "301602218055"
+var studentName = "vaibhaw vaish"
+let userId = "vaishvaibhaw@gmail.com"
+let password = "vaish@123"
+city = "Raipur"
+//collegeId = "MHC2023008"
+
+console.log(collegeId);
