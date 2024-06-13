b27b2b6643e400654367bf34bd0c1337873840a3
CashV <101281150+CashV@users.noreply.github.com>
2024-06-13 19:27:29 +0530

    Delete variables.js

--- variables.js
+++
@@ -1,8 +0,0 @@
-const collegeId = "301602218055"
-var studentName = "vaibhaw vaish"
-let userId = "vaishvaibhaw@gmail.com"
-let password = "vaish@123"
-city = "Raipur"
-//collegeId = "MHC2023008"
-
-console.log(collegeId);
