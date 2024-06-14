ba26b3318ca1b7533616a11c86f66d338745a1ab
CashV <101281150+CashV@users.noreply.github.com>
2024-06-14 05:58:37 +0000

    config done

---
+++ basics/04_string.js
@@ -0,0 +1,10 @@
+const name = "vaibhaw"
+const age = "23"
+const gameName = new String('VAIBHAW VAISH')
+console.log(`my name is ${name} and i am currently ${age} year old`);
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toLowerCase());
+console.log(gameName.trim());
