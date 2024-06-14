b5903b19e6a8713cbc9fba74f27118dc9090e60f
CashV <101281150+CashV@users.noreply.github.com>
2024-06-14 06:53:38 +0000

    config done

---
+++ basics/05_numsAndMath.js
@@ -0,0 +1,17 @@
+const score = 400
+const balance = new Number(12400)
+//console.log(balance);
+//console.log(typeof score);
+//console.log(balance.toString());
+//console.log(balance.toString().length);
+//console.log(typeof balance);
+
+//+++++++++++++++++maths++++++++++++++++++++++++//
+
+console.log(Math.floor(Math.random()));
+console.log(Math.floor(Math.random()*10));
+console.log(Math.floor(Math.random()*10)+1);
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
