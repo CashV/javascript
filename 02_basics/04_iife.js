77246544448af3c495d44dd15a24af3c4e81596a
CashV <101281150+CashV@users.noreply.github.com>
2024-06-17 06:51:43 +0000

    config done

--- 02_basics/04_iife.js
+++ 02_basics/04_iife.js
@@ -1,3 +1,22 @@
 //global scope ke pollution se problem hoti hai bahut baar toh uss problem ko hatane ke liye humne iife ka use kiya
 //ye problem mainly function declaration ke time hoti hai
 
+//Imediately Invoked Function Expressions (IIFE)
+
+function database1(){
+    console.log('new database');
+}
+//database1()
+
+(function Database2(){              //named IIFE
+    console.log('setting up database');
+})();       //semicolon is very important for it, as it is stop execution for the flow of function calling otherwise it get also invoked next function immediatly after the first one. which give us error
+
+
+(()=>{
+    console.log('database is connected');
+})();
+
+((name)=>{                  //unnamed IIFE
+    console.log(`database is connected, ready ${name}`);
+})("vaibhaw");
---
+++ 02_basics/05_execution_context.js
@@ -0,0 +1,1 @@
+//javascipt  execution context
